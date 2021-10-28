4f41bcd14271c6333038edc18a909a44fb161b84
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-10-27 21:21:51 -0400

    set up

--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const path = require("path");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
+
+const usersRouter = require("./users/users-router");
+const authRouter = require("./auth/auth-router");
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -17,15 +23,46 @@ const cors = require("cors");
 
 const server = express();
 
+const config = {
+  name: "sessionId",
+  secret: "keep it secret, keep it safe",
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUnitialized: false,
+  store: new KnexSessionStore({
+    knex: require("../database/db-config.js"),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createTable: true,
+    clearInterval: 1000 * 60 * 60,
+  }),
+};
+
+server.use(express.static(path.join(__dirname, "../client")));
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use(session(config));
+
+server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
+
+server.use("*", (req, res, next) => {
+  next({ status: 404, message: "not found!" });
+});
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use((err, req, res, next) => {
+  // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
